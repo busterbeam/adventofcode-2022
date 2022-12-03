1fac6915c16a3c86bcd8a4a0e978229534f958c6
Nathan <nathanlloyd@protonmail.com>
2022-12-03 13:39:43 +0000

    Day 3 complete on Saturday the 3rd of December.  It has 4 functions and imports `strings.ascii_letters` to make the `item_priority` function a one liner, by simply checking the index of the variable and incrementing the index by one as the puzzle uses indexing that starts at 1.  `sum_of_rucksack_priorities` and `sum_of_group_priorities` functions are to solve for the different puzzle selections.  `rucksack_input` simply handles the input based on what we want to read from it.

--- day_3.py
+++ day_3.py
@@ -5,30 +5,58 @@
 from string import ascii_letters
 
 
-def rucksack_input():
+def rucksack_input(group=False):
 	rucksacks = list()
-	print("The Rucksack contents:\nPlease enter empty line to finish input")
+	variable = "Group" if group else "Rucksack"
+	print("The {variable} contents:\nPlease enter empty line to finish input\n")
+	group_index = 0
 	while True:
 		line = input()
 		if line:
 			line = line.strip()
-			rucksacks.append((line[:len(line)//2], line[len(line)//2:]))
+			if group:
+				if group_index % 3 == 0:
+					rucksacks.append(list())
+				rucksacks[-1].append(
+					(line[:len(line)//2], line[len(line)//2:]))
+			else:
+				rucksacks.append(
+					(line[:len(line)//2], line[len(line)//2:]))
 		else:
 			break
+		group_index += 1
 	return rucksacks
 
 
 def item_priority(item):
 	return ascii_letters.index(item) + 1
-	
 
-if __name__ == "__main__":
-	rucksacks = rucksack_input()
+
+def sum_of_rucksack_priorities(rucksacks):
 	priority_sum = 0
 	for num, rucksack in enumerate(rucksacks, 1):
 		for item in rucksack[0]:
 			if item in rucksack[1]:
 				priority_sum += item_priority(item)
 				break
-	print(priority_sum)
+	return priority_sum
+
+
+def sum_of_group_priorities(groups):
+	priority_sum = 0
+	for num, group in enumerate(groups, 1):
+		for item in str.join(*group[0]):
+			if item in str.join(*group[1]) and item in str.join(*group[2]):
+				priority_sum += item_priority(item)
+				break
+	return priority_sum
+
+
+if __name__ == "__main__":
+	if input("Rucksack or Group priority summing? r/g") == "r":
+		rucksacks = rucksack_input(group=False)
+		print(sum_of_rucksack_priorities(rucksacks))
+	else:
+		rucksacks = rucksack_input(group=True)
+		print(sum_of_group_priorities(rucksacks))
 
