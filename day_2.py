83124ad303a74411c70dbaa5c8f8d282bc88ab42
Nathan <nathanlloyd@protonmail.com>
2022-12-03 13:01:48 +0000

    Now Day 2 is complete, I keep forgetting about the "Part 2"

--- day_2.py
+++ day_2.py
@@ -10,16 +10,25 @@ def strategy_guide_input():
 	while True:
 		line = input()
 		if line:
-			strategies.append(line.split(' '))
+			strategies.append(line.strip().split(' '))
 		else:
 			break
 	return list(filter(None, strategies))
 
-scoring = {	
+METRIC = {	
 	"X": 1, "A": 1, # Rock
 	"Y": 2, "B": 2, # Paper
 	"Z": 3, "C": 3, # Scissors
 }
+
+
+def win_or_lose(opponent, you):
+	if opponent == "A":
+		return 3 if you == "X" else (6 if you == "Y" else 0)
+	elif opponent == "B":
+		return 0 if you == "X" else (3 if you == "Y" else 6)
+	else:
+		return 6 if you == "X" else (0 if you == "Y" else 3)
 
 
 if __name__ == "__main__":
@@ -27,11 +36,12 @@ if __name__ == "__main__":
 	score = 0
 	for strategy in strategies:
 		opponent, you = strategy
-		if opponent == "A":
-			score += 3 if you == "X" else (6 if you == "Y" else 0)
-		elif opponent == "B":
-			score += 0 if you == "X" else (3 if you == "Y" else 6)
-		else:
-			score += 6 if you == "X" else (0 if you == "Y" else 3)
-		score += scoring[you]
+		result = 0 if you == "X" else (3 if you == "Y" else 6)
+		for move in ["X", "Y", "Z"]:
+			if result == win_or_lose(opponent, move):
+				you = move
+				break
+		score += METRIC[you] + result
+		
 	print(score)
+
